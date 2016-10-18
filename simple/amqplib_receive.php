78d67711ba819d8172b9340980a1c495e4f7b5fa
kongwenhua <kongwenhua@yaochufa.com>
2016-10-18 15:46:17 +0800

    change

--- simple/amqplib_receive.php
+++ simple/amqplib_receive.php
@@ -2,6 +2,8 @@
 require_once __DIR__ . '/../vendor/autoload.php';
 $config = require_once __DIR__ . '/../config.php';
 
+use PhpAmqpLib\Connection\AMQPStreamConnection;
+use PhpAmqpLib\Message\AMQPMessage;
 $connection = new AMQPStreamConnection($config['rm_server'], $config['rm_port'], $config['rm_user'], $config['rm_pwd']);
 $channel = $connection->channel();
 
