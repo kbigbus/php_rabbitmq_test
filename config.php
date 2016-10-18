8125db4fc7b83c2a1c4de26c426fbc98a62605a9
kongwenhua <kongwenhua@yaochufa.com>
2016-10-18 15:43:56 +0800

    commit

--- config.php
+++ config.php
@@ -1,8 +1,16 @@
 <?php
 
 return [
-	'rm_server' => '192.168.19.123',
+	// 'rm_server' => '192.168.19.123',
+	// 'rm_port' => 5672,
+	// 'rm_user' => 'guest',
+	// 'rm_pwd' => 'guest',
+	// 'rm_vhost' => '/',
+
+	'rm_server' => '192.168.9.24',
 	'rm_port' => 5672,
-	'rm_user' => 'guest',
-	'rm_pwd' => 'guest'
+	'rm_user' => 'php_test',
+	'rm_pwd' => 'php_test',
+	'rm_vhost' => 'php',
+
 ];
