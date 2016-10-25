dfb8cb5d43e2d12742ddf15d898e9e88da97b129
kongwenhua <kongwenhua@yaochufa.com>
2016-10-25 15:31:57 +0800

    edit

--- README.md
+++ README.md
@@ -16,3 +16,15 @@ php-amqplib 版本 2.5.*
 
 这是由于测试时另一个脚本用了 hello 这个队列导致的，改另一个名就行了。  补上网络上的解答（一般原因：建立channel或queue的参数不同。特别是declare已有的同名queue时容易出现此错误。解决办法：删除rabbitmq-server上相应的queue，重新建立）<p>
 
+##队列处理说明（p producer生成者 c consumer消费者）
+###简单队列
+一一对应关系 设置好对应的routekey 1p to 1c
+
+###一对多
+1p to Nc 一个生成者对应多个消费者， 需要确认的问题有 <br>
+1、多进程的消息分配问题（rabbitmq自己解决）
+2、消息丢失恢复问题（一个消费者处理过程中挂掉了，保证当前这条消息能恢复处理。消息确认机制（ack/nack））
+3、rabbitmq 服务器挂掉（消息持久化durable，能解决大部分问题。 If you need a stronger guarantee then you can use publisher confirms.）
+4、多条复杂任务分配给一个c，多条简单任务分配给一个c。造成一个很忙一个很闲（消息公平调度，AMQPChannel::qos）
+
+###订阅
---
+++ workqueue/amqplib_send.php
@@ -0,0 +1,25 @@
+<?php
+require_once __DIR__ . '/../vendor/autoload.php';
+$config = require_once __DIR__ . '/../config.php';
+
+use PhpAmqpLib\Connection\AMQPStreamConnection;
+use PhpAmqpLib\Message\AMQPMessage;
+
+$connection = new AMQPStreamConnection($config['rm_server'], $config['rm_port'], $config['rm_user'], $config['rm_pwd'], $config['rm_vhost']);
+$channel = $connection->channel();
+
+$channel->queue_declare('task_queue', false, true, false, false);
+
+$data = implode(' ', array_slice($argv, 1));
+if(empty($data)) $data = "Hello World!";
+$msg = new AMQPMessage($data,
+                        array('delivery_mode' => 2) # make message persistent
+                      );
+
+$channel->basic_publish($msg, '', 'task_queue');
+
+echo " [x] Sent ", $data, "\n";
+
+$channel->close();
+$connection->close();
+
