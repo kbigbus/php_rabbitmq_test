cd689711c8543241fa65afbf99ca7176688bc93c
kongwenhua <kongwenhua@yaochufa.com>
2016-11-15 18:37:04 +0800

    confirm test

--- route/amqplib_send.php
+++ route/amqplib_send.php
@@ -8,6 +8,20 @@ use PhpAmqpLib\Message\AMQPMessage;
 $connection = new AMQPStreamConnection($config['rm_server'], $config['rm_port'], $config['rm_user'], $config['rm_pwd'], $config['rm_vhost']);
 $channel = $connection->channel();
 
+$channel->set_ack_handler(
+    function (AMQPMessage $message) {
+        echo "Message acked with content " . $message->body . PHP_EOL;
+    }
+);
+
+$channel->set_nack_handler(
+    function (AMQPMessage $message) {
+        echo "Message nacked with content " . $message->body . PHP_EOL;
+    }
+);
+
+$channel->confirm_select();
+
 $channel->exchange_declare('direct_logs', 'direct', false, false, false);
 
 $severity = isset($argv[1]) && !empty($argv[1]) ? $argv[1] : 'info';
@@ -20,6 +34,7 @@ $msg = new AMQPMessage($data);
 $channel->basic_publish($msg, 'direct_logs', $severity);
 
 echo " [x] Sent ",$severity,':',$data," \n";
+$channel->wait_for_pending_acks_returns();
 
 $channel->close();
 $connection->close();
--- simple/amqplib_send.php
+++ simple/amqplib_send.php
@@ -7,12 +7,28 @@ use PhpAmqpLib\Message\AMQPMessage;
 
 $connection = new AMQPStreamConnection($config['rm_server'], $config['rm_port'], $config['rm_user'], $config['rm_pwd'], $config['rm_vhost']);
 $channel = $connection->channel();
+$channel->set_ack_handler(
+    function (AMQPMessage $message) {
+        echo "Message acked with content " . $message->body . PHP_EOL;
+    }
+);
+
+$channel->set_nack_handler(
+    function (AMQPMessage $message) {
+        echo "Message nacked with content " . $message->body . PHP_EOL;
+    }
+);
+
+$channel->confirm_select();
+
 $channel->queue_declare('world', false, false, false, false);
 
 $msg = new AMQPMessage('Hello World!', array('correlation_id'=>uniqid()));
 $channel->basic_publish($msg, '', 'world');
 
 echo " [x] Sent 'Hello World!'\n";
+
+$channel->wait_for_pending_acks_returns();
 
 $channel->close();
 $connection->close();
